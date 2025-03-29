f964b82796f2418775dca95005febe49d5ef1765
yuran1811 <trieuvanbd123@gmail.com>
2025-03-29 23:05:25 +0700

    feat: ✨ add glow components

--- operating-system/lab2/23127065-Code/app/pages/components-test.vue
+++ operating-system/lab2/23127065-Code/app/pages/components-test.vue
@@ -8,19 +8,24 @@ definePageMeta({
 
 <template>
   <LayoutDefault>
-    <!-- <div class="pattern-container" /> -->
-    <!-- <CursorFluid /> -->
+    <section class="space-y-12 pb-24">
+      <!-- <div class="pattern-container" /> -->
 
-    <ParallaxCard />
+      <!-- <CursorFluid /> -->
 
-    <div
-      class="xs:grid-cols-2 container mx-auto grid max-w-md grid-cols-1 items-center justify-items-center gap-12 pb-32"
-    >
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
+      <GlowGrid />
+
+      <ParallaxCard />
+
+      <div
+        class="xs:grid-cols-2 container mx-auto grid max-w-md grid-cols-1 items-center justify-items-center gap-12"
+      >
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </section>
 
     <Dock />
   </LayoutDefault>
